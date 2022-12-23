0bf00b45d0c1b42f36df501aa092d750a4fee582
Ayeesha839 <121211621+Ayeesha839@users.noreply.github.com>
2022-12-23 20:23:55 +0530

    Update program-01.py

--- program-01.py
+++ program-01.py
@@ -5,4 +5,4 @@ else:
     pi=3.14159
     radius=round((inpArea/pi)**(1/2),6)
     print("for the given area",inpArea,"radius is:",radius)
-    print("Area:"+str(inpArea),"radius:"+str(radius))
+    print("Area:"+str(inpArea),"radius:"+str(radius))   
