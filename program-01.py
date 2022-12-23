3f28d35a7b0db434bcaa9f222f8fced45889d482
Ayeesha839 <121211621+Ayeesha839@users.noreply.github.com>
2022-12-23 20:19:12 +0530

    Add files via upload

---
+++ program-01.py
@@ -0,0 +1,8 @@
+inpArea=int(input("Enter tha area of the circle:"))
+if inpArea<0:
+    print('Invalid area')
+else:
+    pi=3.14159
+    radius=round((inpArea/pi)**(1/2),6)
+    print("for the given area",inpArea,"radius is:",radius)
+    print("Area:"+str(inpArea),"radius:"+str(radius))
