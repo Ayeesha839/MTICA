f68f0296b4577760e8b989fd8b4443fe8a4e01f4
Ayeesha839 <121211621+Ayeesha839@users.noreply.github.com>
2022-12-26 17:55:36 +0530

    Add files via upload

---
+++ Day5shell.py
@@ -0,0 +1,333 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-01.py ==
+256
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-01.py ==
+256
+-8
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-02.py ==
+Hello world
+value of x is 8 value of y is 10
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-03.py ==
+This
+is a multi-line
+string.
+s1="""This
+is a multi-line
+string."""
+s1
+'This\nis a multi-line\nstring.'
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-04.py ==
+s0:
+ *	*
+**	**
+***	***
+
+s1:
+ I said,"This is valid string."
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-04.py ==
+/ts0:
+ *	*
+**	**
+***	***
+
+s1:
+ I said,"This is valid string."
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-04.py ==
+s0:
+ /t*	*
+**	**
+***	***
+
+s1:
+ I said,"This is valid string."
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-04.py ==
+s0:
+ *	*
+**	**
+***	***
+
+s1:
+ I said,"This is valid string."
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-05.py ==
+s1
+'"Hi mom",I said. "How are you?"'
+s2
+'"Hi mom",I said. How are you?'
+s4
+'\'Hi mom",I said. \'"How are you?"\''
+s6
+'"Is this a cheese shop?"\n\t" we have all kinds!"'
+
+=== RESTART: D:/pythonpractice06/day5/Day5program-05.py ==
+"Hi mom",I said. "How are you?"
+
+=== RESTART: D:/pythonpractice06/day5/Day5Output-06.py ===
+cats	are
+ 	good	sources
+		of	internet	memes
+s="cats\tare\n \tgood\tsources\n\t\tof\tinternet\tmemes"
+s
+SyntaxError: multiple statements found while compiling a single statement
+s="cats\tare\n \tgood\tsources\n\t\tof\tinternet\tmemes"
+s
+'cats\tare\n \tgood\tsources\n\t\tof\tinternet\tmemes'
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-01.py ==
+mca cam
+Yes
+mca dam
+SyntaxError: invalid syntax
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-01.py ==
+mca dam
+No
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-02.py ==
+amma mama
+Traceback (most recent call last):
+  File "D:/pythonpractice06/day5/Day5Program-02.py", line 7, in <module>
+    print(chekPalindrome(inp))
+  File "D:/pythonpractice06/day5/Day5Program-02.py", line 2, in chekPalindrome
+    if sorted(s1)==sorted(s2):
+NameError: name 's1' is not defined
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-02.py ==
+amma mama
+No
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-02.py ==
+amma mama
+No
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-02.py ==
+amma
+Yes
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-02.py ==
+54545
+Yes
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-02.py ==
+5435
+No
+
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-03.py ==
+my name is Ayeesha
+Vowels: 
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-03.py ==
+my name is ayeesha
+Vowels: 
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-03.py ==
+my name is Ayeesha
+Vowels: 
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-03.py ==
+my name is Ayeesha
+Vowels: aeiAeea
+
+=== RESTART: D:\pythonpractice06\day5\Day5output-07.py ===
+\\\\
+\
+\
+\
+
+Good-bye
+
+=== RESTART: D:\pythonpractice06\day5\Day5output-08.py ===
+abcdef
+
+=== RESTART: D:\pythonpractice06\day5\Day5output-08.py ===
+abcdef
+abcdef
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+A short event follow soon
+No of Vowels in:' A short event follow soon ' is Aoeeoooo
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+A short event follow soon
+Number of Vowels in:' A short event follow soon ' is Aoeeoooo
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+A short event follow soon
+Traceback (most recent call last):
+  File "D:/pythonpractice06/day5/Day5Program-04.py", line 8, in <module>
+    a=count_vowel(str1)
+  File "D:/pythonpractice06/day5/Day5Program-04.py", line 5, in count_vowel
+    n_vowel+=i
+TypeError: unsupported operand type(s) for +=: 'int' and 'str'
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+A short event follow soon
+Number of Vowels in:' A short event follow soon ' is 8
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-04.py ==
+A short event will follow soon
+Number of Vowels in:' A short event will follow soon ' is 9
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-05.py ==
+234
+digits: 234
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-05.py ==
+235hgghds676678cvbhc
+digits: 235676678
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-06.py ==
+123dfg45
+Traceback (most recent call last):
+  File "D:/pythonpractice06/day5/Day5Program-06.py", line 8, in <module>
+    a=count_digit(str1)
+  File "D:/pythonpractice06/day5/Day5Program-06.py", line 5, in count_digit
+    n_digit+=1
+UnboundLocalError: cannot access local variable 'n_digit' where it is not associated with a value
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-06.py ==
+12345
+Traceback (most recent call last):
+  File "D:/pythonpractice06/day5/Day5Program-06.py", line 8, in <module>
+    a=count_digit(str1)
+  File "D:/pythonpractice06/day5/Day5Program-06.py", line 5, in count_digit
+    n_digit+=1
+UnboundLocalError: cannot access local variable 'n_digit' where it is not associated with a value
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-06.py ==
+1234
+Number of digits in:' 1234 ' is 4
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-07.py ==
+@#fdt123&*
+specialcharacters: @#&*
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-08.py ==
+!@#hgsa667&*
+Number of specialcharacters in:' !@#hgsa667&* ' is 5
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-09.py ==
+Ayeesha
+consonants: ysh
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-09.py ==
+My name is Ayeesha
+consonants: Mynmsysh
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-10.py ==
+My name is Ayeesha
+Number of consonants in:' My name is Ayeesha ' is 8
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-06.py ==
+dfg654
+Number of digits in:' dfg654 ' is 3
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-07.py ==
+df.,=46577&$
+specialcharacters: df46577
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-08.py ==
+hjv';./
+Number of specialcharacters in:' hjv';./ ' is 4
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-07.py ==
+hjv';./
+specialcharacters: ';./
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-11.py ==
+Today
+string Today withou vowel is Tdy
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-07.py ==
+dg&*14$}
+specialcharacters: &*$}
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-07.py ==
+dg&*14$}
+specialcharacters: &*$}
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-07.py ==
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-12.py ==
+Enter your text:my name is ayeesha
+Without duplicate: my naeish
+
+=== RESTART: D:/pythonpractice06/day5/Day5Program-12.py ==
+Enter your text:Andhrapradesh
+Without duplicate: Andhrapes
+Without duplicate: Andhrapes
+SyntaxError: invalid syntax
+'apple'*3
+'appleappleapple'
+a=7
+'*'*a
+'*******'
+print('*'*a)
+*******
+b='today'  "is" 'monday'
+b
+'todayismonday'
+c=b='today' + "is" + 'monday'
+c
+'todayismonday'
+print('today'  "is" 'monday')
+todayismonday
+
+=== RESTART: D:\pythonpractice06\day5\Day5Program-08.py ==
+as%&$24
+Number of specialcharacters in:' as%&$24 ' is 3
+for i in range(10):print(i)
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+for i in range(10):print(i,end='#')
+
+0#1#2#3#4#5#6#7#8#9#
+for i in range(10):print(i,end=' ')
+
+0 1 2 3 4 5 6 7 8 9 
+for i in range(10):print(i,sep=' ')
+
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+for i in range(10):print(i,end=',')
+
+0,1,2,3,4,5,6,7,8,9,
+a=5
+b=7
+print(a,'+',b,'=',c)
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    print(a,'+',b,'=',c)
+NameError: name 'c' is not defined
+print(a,'+',b,'=',c)
