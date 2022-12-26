ee3765bb5c00e7541348b5ee8387dbc7fac6080c
Ayeesha839 <121211621+Ayeesha839@users.noreply.github.com>
2022-12-26 16:41:54 +0530

    Add files via upload

---
+++ Day4program-05.py
@@ -0,0 +1,16 @@
+####def replace6by0(n):
+####    n=str(n)
+####    n=n.replace('6','0')
+####    return n
+####inp=int(input())
+####print(replace6by0(inp))
+
+
+
+
+def replace1by0(n):
+    n=str(n)
+    n=n.replace('1','0')
+    return n
+inp=int(input())
+print(replace1by0(inp))
