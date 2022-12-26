f152c9ed365e42388602dc2e1ec097c15b175558
Ayeesha839 <121211621+Ayeesha839@users.noreply.github.com>
2022-12-26 17:22:07 +0530

    Add files via upload

---
+++ Day5output-09.py
@@ -0,0 +1,13 @@
+Python 3.11.0a5 (main, Feb  3 2022, 19:32:53) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print(len('george'))
+6
+print(len(' tom  '))
+6
+s="""
+sis!
+"""
+print(len(s))
+6
+s
+'\nsis!\n'
